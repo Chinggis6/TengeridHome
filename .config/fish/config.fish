be2b864358dcdf21987e4483d835362bbd73b95e
Chinggis6 <Chinggis6@iCloud.com>
2017-03-19 09:55:31 +0400

    Bulk commit :fish:

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+# Vi modes for command line
+set fish_vi_keybindings
+
+# Remove greeting
+set fish_greeting ''
+
+# Source environment variables
+source "$HOME/.config/fish/export.fish"
+
+# Source aliases
+source "$HOME/.config/fish/alias"
